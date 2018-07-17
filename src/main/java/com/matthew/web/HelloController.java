e114e17fd50ad9bf2f001594826094401efc7dea
MatthewLQM <MatthewLQM@163.com>
2018-07-17 23:59:10 +0800

    加入不同环境的配置化文件

--- src/main/java/com/matthew/web/HelloController.java
+++ src/main/java/com/matthew/web/HelloController.java
@@ -1,13 +1,22 @@
 package com.matthew.web;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${com.matthew.project.author.name}")
+    private String authorName;
+
+    @Value("${com.matthew.project.author.email}")
+    private String authorEmail;
+
     @RequestMapping("/hello")
     public String index() {
-        return "Hello World";
+        StringBuilder stringBuilder = new StringBuilder("Hello World\r\n");
+        stringBuilder.append("author name: " + authorName + "\r\n").append("author email: " + authorEmail + "\r\n");
+        return stringBuilder.toString();
     }
 }
